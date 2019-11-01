0d25058c2e56cb10c550487992498d6cb0f3b993
hendrymasiku <hendrymasiku24@gmail.com>
2019-11-01 18:48:53 +0700

    Preference

--- app/src/main/java/com/example/proteintrackerapp/MainActivity.java
+++ app/src/main/java/com/example/proteintrackerapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.proteintrackerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnSetting = (Button) findViewById(R.id.btnSetting);
+        btnSetting.setOnClickListener(myBtnSettingClick);
+
+        SharedPreferences prefs =
+                MainActivity.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+        String statusLogin = prefs.getString("isLogin",null);
+        Button btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(myBtnLoginClick);
+        if (statusLogin != null){
+            btnLogin.setText("Logout");
+        }else{
+            btnLogin.setText("Login"); }
     }
+
+    private View.OnClickListener myBtnSettingClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        }
+
+        ;
+    };
+
+    private View.OnClickListener myBtnLoginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SharedPreferences prefs = MainActivity.this.getSharedPreferences("prefs_file", MODE_PRIVATE);
+            String statusLogin = prefs.getString("isLogin", null);
+            SharedPreferences.Editor edit = prefs.edit();
+            Button btnLogin = (Button) findViewById(R.id.btnLogin);
+            if (statusLogin != null) {
+                edit.putString("isLogin", null);
+                btnLogin.setText("Login");
+            } else {
+                edit.putString("isLogin", "Admin");
+                btnLogin.setText("Logout");
+            }
+            edit.commit();
+        }
+    };
 }
+
+
